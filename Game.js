451d68b1c6ef7c8449c34e4786637425e2ff71b4
Phamlam <mailam1309@gmail.com>
2021-06-04 22:58:26 +0700

    change

--- Game.js
+++ Game.js
@@ -1,6 +1,7 @@
 function Validate(options){
     var text_img = document.querySelector(options.text_img);
     var back_to_top = document.querySelector(options.back_to_top);
+    var new_info = document.querySelector(options.new_info);
 
     setTimeout(function(){
         $(text_img).css("opacity", "1");
@@ -11,6 +12,19 @@ function Validate(options){
     $(back_to_top).on("click",function () { 
         console.log("ok");
         $('html,body').animate({scrollTop: 0},1000);
-        
+    });
+
+    $(window).scroll(function (e) { 
+        var pos_body = $('html,body').scrollTop();
+        console.log(pos_body);
+        if(pos_body >= 600 ){
+            $(new_info).css("visibility", "visible");
+            $(new_info).css("transform", "translateX(35%)");     
+        }
+        else if(pos_body < 700){
+            $(new_info).css("visibility", "hidden");
+            $(new_info).css("transform", "translateX(-35%)");  
+        }
+       
     });
 }
